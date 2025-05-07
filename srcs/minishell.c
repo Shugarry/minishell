503b0b9e01d3e9c70c73e38d8748844ee457176f
Miguel Antonio Garcia <miggarc2@cbr8s2.42barcelona.com>
2025-05-07 22:48:33 +0200

    execve implemented - without pipes yet - pend clean comments - and pend study other functions

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: miggarc2 <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 19:02:08 by miggarc2          #+#    #+#             */
-/*   Updated: 2025/05/07 20:11:43 by miggarc2         ###   ########.fr       */
+/*   Updated: 2025/05/07 22:34:38 by miggarc2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 int	ft_perror(char *err1, char *err2, char *err3, int err_no)
 {
-	ft_putstr_fd("pipex: ", STDERR_FILENO);
 	ft_putstr_fd(err1, STDERR_FILENO);
 	ft_putstr_fd(err2, STDERR_FILENO);
 	ft_putstr_fd(err3, STDERR_FILENO);
@@ -61,7 +60,8 @@ void	ft_exit(t_var *var, int exit_code)
 
 void	ft_exec_child(t_var *var, int i, int end, char **env)
 {
-	if (i < end)
+	(void)end;
+/*	if (i < end)
 		close(var->pipes[2 * i]);
 	if (i == 0 && dup2(var->fd_in, STDIN_FILENO) < 0)
 		ft_exit(var, 1);
@@ -73,8 +73,8 @@ void	ft_exec_child(t_var *var, int i, int end, char **env)
 		ft_exit(var, 1);
 	if (var->cmds[i][0] == NULL)
 		ft_exit(var, ft_perror("", "permission denied: ", "", 126));
-	execve(var->cmds[i][0], var->cmds[i], env);
-	ft_exit(var, ft_perror("command not found: ", var->cmds[i][0], "", 127));
+*/	execve(var->cmds[i][0], var->cmds[i], env);
+	ft_exit(var, ft_perror(var->cmds[i][0], ": command not found", "", 127));
 }
 
 int	ft_pipex(t_var *var, int end, char **env, int exit_code)
@@ -86,22 +86,22 @@ int	ft_pipex(t_var *var, int end, char **env, int exit_code)
 	i = -1;
 	while (var->cmds[++i])
 	{
-		if (i < end && pipe(&var->pipes[2 * i]) < 0)
-			ft_exit(var, ft_perror("", strerror(errno), "", errno));
+//		if (i < end && pipe(&var->pipes[2 * i]) < 0)
+//			ft_exit(var, ft_perror("", strerror(errno), "", errno));
 		child = fork();
 		if (child < 0)
 			ft_exit(var, ft_perror("", strerror(errno), "", errno));
 		else if (child == 0)
 			ft_exec_child(var, i, end, env);
-		if (i < end)
-			close(var->pipes[2 * i + 1]);
+//		if (i < end)
+//			close(var->pipes[2 * i + 1]);
 	}
-	while (i)
+	while (i-- >= 0)
 	{
 		if (waitpid(-1, &status, 0) == child && WIFEXITED(status))
 			exit_code = WEXITSTATUS(status);
-		if (--i)
-			close(var->pipes[2 * (i - 1)]);
+//		if (--i)
+//			close(var->pipes[2 * (i - 1)]);
 	}
 	return (exit_code);
 }
