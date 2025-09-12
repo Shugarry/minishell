a30151346d95241cdf7c38644e3080505394d7b3
shugarry <114780801+Shugarry@users.noreply.github.com>
2025-09-12 20:40:06 +0200

    changing .h

--- minishell.h
+++ minishell.h
@@ -102,33 +102,25 @@ void	*memlist_alloc(t_var *var, size_t size);
 void	*memlist_add(t_var *var, void *ptr);
 void	memlist_free_ptr(t_var *var, void *ptr);
 
-// BUILTINS
-
 // minishell_builtins_extra.c
 void	ms_echo(char **tokens);
 char	*getcwd_plus(t_var *var);
 void	ms_pwd(t_var *var);
 void	ms_exit_builtin(t_var *var, int i);
-// minishell_builtins_chdir.c
-//static int	bad_status(int status);
-//static void	cd_home(t_var *var);
-//static void	cd_previous(t_var *var);
-//static void	cd_todir(t_var *var, char **tokens)
 void	ms_cd(t_var *var, char **tokens);
+
 // minishell_builtins_vars.c
-//static void	export_print(t_var *var);
-//static void	export_add(t_var *var, char *token)
 void	ms_export(t_var *var, char **tokens);
 void	ms_unset(t_var *var, char **tokens);
 void	ms_env(t_var *var, char **tokens);
 
-// ENVIRONMENT VARIABLES
-
 // minishell_env_init.c
 void	modify_env_var(t_var *var, char *var_name, char *new_content);
+
 //static bool	validate_shlvl(char *shlvl)
 void	add_shlvl(t_var *var, char *shlvl);
 void	create_env(t_var *var, char **env);
+
 // minishell_env_helpers.c
 void	varlist_clean(t_var	*var);
 void	add_env_var(t_var *var, char *variable);
@@ -136,9 +128,7 @@ bool	find_env_var(t_var *var, char *var_name);
 void	remove_env_var(t_var *var, char *var_name);
 char	*get_env_var(t_var *var, char *variable);
 
-// VARIABLE EXPANSION
 // minishell_expansion.c
-//static void token_size_logic(t_var *var, char *token, int *len, t_builder *b)
 int		new_token_size(t_var *var, char *token);
 void	logic_quote_case(char *token, int *len, t_builder *b);
 bool	builder_if_helper(char *token, t_builder *b);
